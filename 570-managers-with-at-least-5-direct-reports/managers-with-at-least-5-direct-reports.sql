0df814a559f7f0ed0bb0fee03435a59c04820f6d
Caitlyn O'Connor <caitlynoc@hotmail.com>
2025-03-07 11:30:21 +0000

    Time: 1686 ms (35.85%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,28 +1,3 @@
--- -- select 
--- --     e1.name
--- -- from employee e1
--- -- inner join employee e2
--- --     on e1.id = e2.managerId
--- -- where e1.managerId is null
--- -- group by e1.name
--- -- having count(*)>4
-
--- with cte as (
--- select 
---      e1.name
---      ,count(e1.name) peepeepoopoo
---  from employee e1
---  inner join employee e2
---     on e1.id = e2.managerId
--- group by e1.name, e1.id
--- having count(e1.name)>4
--- )
-
--- select
---     cte.name
--- from cte;
-
-
 with cte as (
 select
     managerId
