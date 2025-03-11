49d7866faf4cee336a5aa354dacb2ff15ac6bb19
Caitlyn O'Connor <caitlynoc@hotmail.com>
2025-03-11 11:08:06 +0000

    Time: 393 ms (89.91%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,6 +1,7 @@
 select 
     p.product_id
-    ,coalesce(round(cast(sum(p.price*u.units)as decimal(10,2))/cast(sum(u.units) as decimal(10,2)),2),0) as average_price
+    ,coalesce(round(cast(sum(p.price*u.units)as decimal(10,2))
+    /cast(sum(u.units) as decimal(10,2)),2),0) as average_price
 from prices p
 left join UnitsSold u
     on p.product_id = u.product_id and u.purchase_date between p.start_date and p.end_date
